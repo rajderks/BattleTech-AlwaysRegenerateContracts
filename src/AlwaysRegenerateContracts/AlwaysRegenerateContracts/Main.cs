fd4015e096ad556ba022750a02ee303dc13a52f2
rajderks <2419711+rajderks@users.noreply.github.com>
2019-01-16 15:42:54 +0100

    Added logger

--- src/AlwaysRegenerateContracts/AlwaysRegenerateContracts/Main.cs
+++ src/AlwaysRegenerateContracts/AlwaysRegenerateContracts/Main.cs
@@ -6,8 +6,12 @@ namespace AlwaysRegenerateContracts
     {
         public static void Init(string directory, string settingsJSON)
         {
+            AlwaysRegenerateContracts.ModDirectory = directory;
             var harmony = HarmonyInstance.Create("io.github.rajderks.AlwaysRegenerateContracts");
             harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
         }
+
+        internal static string ModDirectory;
     }
+
 }
